ebd01ef1bf8f289de0c65a182183a854f36bdeed
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2023-09-27 20:37:05 -0300

    projeto pronto, excluindo e add produtos. falta parte de CSS

---
+++ Project/src/index.jsx
@@ -0,0 +1,37 @@
+import { useRef, useState } from "react";
+import { v4 } from "uuid";
+
+function Home() {
+  const [product, setProduct] = useState([]);
+  const input = useRef();
+  const addProduct = () => {
+    setProduct([
+      {
+        id: v4(),
+        nome: input.current.value,
+      },
+      ...product,
+    ]);
+    input.current.value = "";
+  };
+  const deleteProduct = (id) => {
+    setProduct(product.filter((product) => product.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Lista de Compras</h1>
+      <input type="text" placeholder="Digite um produto..." ref={input} />
+      <button onClick={addProduct}>Adicionar</button>
+
+      {product.map((item) => (
+        <div key={item.id}>
+          <p>{item.nome}</p>
+          <button onClick={() => deleteProduct(item.id)}>🗑️</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
